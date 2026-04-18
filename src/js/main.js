d8398aeccce092e0b39ff88df16749e59b77de15
Yasamin-E84 <yasaminsoraghi84@gmail.com>
2026-04-18 21:21:55 +0330

    header-desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,11 @@
 import "../css/output.css";
 import Annoying from "./header/annoying";
 import Search from "./header/console";
+import Location from "./header/location";
 import NavBar from "./header/navbar";
 import Setting from "./header/setting";
 Annoying();
 Setting();
 Search();
 NavBar();
-
-import './style.css';
-
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-new Swiper('.mySwiper', {
-  modules: [Navigation, Pagination],
-  loop: true,
-  spaceBetween: 20,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+Location();
